d92c5f55d7b621e8506ed3bb4da334fd8667c002
Steven Olmsted <steven.olm@gmail.com>
2012-02-10 10:23:22 -0500

    Updated API documentation.

---
+++ src/gallery-widget-weak-modality/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "WidgetModality"
+    ],
+    "modules": [
+        "gallery-widget-weak-modality"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-widget-weak-modality",
+            "name": "gallery-widget-weak-modality",
+            "description": "Make widget modality less scary."
+        }
+    ]
+} };
+});
--- src/gallery-widget-weak-modality/js/widget-weak-modality.js
+++ src/gallery-widget-weak-modality/js/widget-weak-modality.js
@@ -1,3 +1,8 @@
+/**
+ * Make widget modality less scary.
+ * @module gallery-widget-weak-modality
+ */
+
 (function (Y) {
     'use strict';
     
@@ -5,6 +10,13 @@
         _widgetModality = Y.WidgetModality,
         _widgetModalityStack = _widgetModality.STACK;
     
+    /**
+     * When set to a truthy value, a modal widget can be closed by clicking on the background mask node.
+     * @attribute weak
+     * @default false
+     * @for WidgetModality
+     * @type Boolean
+     */
     _widgetModality.ATTRS.weak = {
         value: false
     };

e2804d2bc51cde0792a139734871df1bab1febc9
Steven Olmsted <steven.olm@gmail.com>
2012-02-10 00:57:32 -0500

    Added API docs.

---
+++ api/api.js
@@ -0,0 +1,119 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Array",
+        "Composite.Image",
+        "ModelList",
+        "WidgetModality",
+        "Y.Async",
+        "Y.AsyncCommand",
+        "Y.Plugin.AsyncCommandClone",
+        "Y.Plugin.AsyncCommandDelay",
+        "Y.Plugin.AsyncCommandWithhold",
+        "Y.Plugin.AsyncPause",
+        "YQL",
+        "YQLCrypto",
+        "YQLRESTClient",
+        "YUI"
+    ],
+    "modules": [
+        "gallery-array-unnest",
+        "gallery-async",
+        "gallery-async-command",
+        "gallery-async-command-clone",
+        "gallery-async-command-delay",
+        "gallery-async-command-withhold",
+        "gallery-async-pause",
+        "gallery-composite-image",
+        "gallery-delay",
+        "gallery-gravatar-url",
+        "gallery-lazy-load",
+        "gallery-model-list-union",
+        "gallery-widget-weak-modality",
+        "gallery-yql-crypto",
+        "gallery-yql-execute.",
+        "gallery-yql-rest-client",
+        "gallery-yql-rest-client-oauth"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-array-unnest",
+            "name": "gallery-array-unnest",
+            "description": "Utility function that flattens nested arrays."
+        },
+        {
+            "displayName": "gallery-async",
+            "name": "gallery-async"
+        },
+        {
+            "displayName": "gallery-async-command",
+            "name": "gallery-async-command"
+        },
+        {
+            "displayName": "gallery-async-command-clone",
+            "name": "gallery-async-command-clone"
+        },
+        {
+            "displayName": "gallery-async-command-delay",
+            "name": "gallery-async-command-delay"
+        },
+        {
+            "displayName": "gallery-async-command-withhold",
+            "name": "gallery-async-command-withhold"
+        },
+        {
+            "displayName": "gallery-async-pause",
+            "name": "gallery-async-pause"
+        },
+        {
+            "displayName": "gallery-composite-image",
+            "name": "gallery-composite-image"
+        },
+        {
+            "displayName": "gallery-delay",
+            "name": "gallery-delay",
+            "description": "Create a function that doesn't execute immediately when it is called."
+        },
+        {
+            "displayName": "gallery-gravatar-url",
+            "name": "gallery-gravatar-url",
+            "description": "This module provides a function for creating gravatar image urls."
+        },
+        {
+            "displayName": "gallery-lazy-load",
+            "name": "gallery-lazy-load",
+            "description": "A little helper function for when you want to load more modules into an existing Y instance."
+        },
+        {
+            "displayName": "gallery-model-list-union",
+            "name": "gallery-model-list-union",
+            "description": "Creates a model list that is the union of two or more other model lists."
+        },
+        {
+            "displayName": "gallery-widget-weak-modality",
+            "name": "gallery-widget-weak-modality",
+            "description": "Make widget modality less scary."
+        },
+        {
+            "displayName": "gallery-yql-crypto",
+            "name": "gallery-yql-crypto",
+            "description": "This module provides convenient client side access to YQL's crypto functions."
+        },
+        {
+            "displayName": "gallery-yql-execute.",
+            "name": "gallery-yql-execute.",
+            "description": "This module Executes JavaScript remotely."
+        },
+        {
+            "displayName": "gallery-yql-rest-client",
+            "name": "gallery-yql-rest-client",
+            "description": "This module is a REST client."
+        },
+        {
+            "displayName": "gallery-yql-rest-client-oauth",
+            "name": "gallery-yql-rest-client-oauth",
+            "description": "This module is a REST client supporting OAuth signatures."
+        }
+    ]
+} };
+});
